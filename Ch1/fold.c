fd13fdbe9a81f6b5d6375adb2eb7fcc647bdd785
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-03-18 23:19:53 +0000

    1-22 wip

---
+++ Ch1/fold.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define LIMIT 10
+#define WORD_LIMIT 1000
+
+void getNextPart(char getNextWord[], int result[]);
+
+int main() {
+    int length, last;
+    int wordStatistics[2];
+    char nextWord[WORD_LIMIT];
+
+    last = 0;
+    length = 0;
+    while(last != 1) {
+        getNextPart(nextWord, wordStatistics);
+        if (length + wordStatistics[0] + 1 > LIMIT) {
+            printf("\n%s ", nextWord);
+            length = wordStatistics[0] + 1;
+        } else {
+            printf("%s ", nextWord);
+            length += wordStatistics[0] + 1;
+        }
+
+        if (wordStatistics[1] == 1) {
+            last = 1;
+        }
+    }
+
+    return 0;
+}
+
+void getNextPart(char nextWord[], int result[]) {
+    int c, i;
+    i = 0;
+
+    while((c = getchar()) != EOF && c != '\n' && c != ' ') {
+        nextWord[i] = c;
+        i++;
+    }
+
+    if (i == 0) {
+        nextWord[0] = ' ';
+        nextWord[1] = '\0';
+        result[0] = 1;
+        result[1] = 0;
+    }
+
+    nextWord[i] = '\0';
+    result[0] = i;
+    result[1] = c == EOF;
+
+}
