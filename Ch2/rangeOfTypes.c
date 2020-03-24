a34d5dbc01a1030c05feca4e52b46a24e62cd045
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-03-24 20:05:18 +0000

    2.1 revised

--- Ch2/rangeOfTypes.c
+++ Ch2/rangeOfTypes.c
@@ -11,27 +11,25 @@
 #define SHORT_INT 16
 #define INT 32 // or in some cases 16
 #define LONG_INT 32
+enum type { SIGNED, UNSIGNED };
 
 float calculateRange(int bits, int isSigned);
 
 int main() {
-    const int isSigned = 1;
-    const int isNotSigned = 0;
-
-    printf("signed char: \t\t%.0f\t\t%.0f\n", -calculateRange(CHAR_BIT, isSigned), calculateRange(CHAR_BIT, isSigned));
-    printf("unsigned char: \t\t%.0f\t\t%.0f\n", 0.0, calculateRange(CHAR_BIT, isNotSigned));
-    printf("signed short int: \t%.0f\t\t%.0f\n", -calculateRange(SHORT_INT, isSigned), calculateRange(SHORT_INT, isSigned));
-    printf("unsigned short int: \t%.0f\t\t%.0f\n", 0.0, calculateRange(SHORT_INT, isNotSigned));
-    printf("signed int: \t\t%.0f\t%.0f\n", -calculateRange(INT, isSigned), calculateRange(INT, isSigned));
-    printf("unsigned int: \t\t%.0f\t\t%.0f\n", 0.0, calculateRange(INT, isNotSigned));
-    printf("signed long int: \t%.0f\t%.0f\n", -calculateRange(LONG_INT, isSigned), calculateRange(LONG_INT, isSigned));
-    printf("unsigned long int: \t%.0f\t\t%.0f\n", 0.0, calculateRange(LONG_INT, isNotSigned));
+    printf("signed char: \t\t%.0f\t\t%.0f\n", -calculateRange(CHAR_BIT, SIGNED), calculateRange(CHAR_BIT, SIGNED));
+    printf("unsigned char: \t\t%.0f\t\t%.0f\n", 0.0, calculateRange(CHAR_BIT, UNSIGNED));
+    printf("signed short int: \t%.0f\t\t%.0f\n", -calculateRange(SHORT_INT, SIGNED), calculateRange(SHORT_INT, SIGNED));
+    printf("unsigned short int: \t%.0f\t\t%.0f\n", 0.0, calculateRange(SHORT_INT, UNSIGNED));
+    printf("signed int: \t\t%.0f\t%.0f\n", -calculateRange(INT, SIGNED), calculateRange(INT, SIGNED));
+    printf("unsigned int: \t\t%.0f\t\t%.0f\n", 0.0, calculateRange(INT, UNSIGNED));
+    printf("signed long int: \t%.0f\t%.0f\n", -calculateRange(LONG_INT, SIGNED), calculateRange(LONG_INT, SIGNED));
+    printf("unsigned long int: \t%.0f\t\t%.0f\n", 0.0, calculateRange(LONG_INT, UNSIGNED));
 
     return 0;
 }
 
 float calculateRange(int bits, int isSigned) {
-    if (isSigned == 1) {
+    if (isSigned == SIGNED) {
         return pow(2, bits - 1) - 1;
     }
 
