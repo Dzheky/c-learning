f1e26d1aac39732a78556d5988b50a0eb1de9b20
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-05-07 20:57:05 +0100

    4.8, 4.9

--- Ch4/getchUngetch.c
+++ Ch4/getchUngetch.c
@@ -4,15 +4,21 @@
 
 char buf[BUFSIZE];
 int bufp = 0;
+int singleBuf = 0;
 
 int getch(void) {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+    if (singleBuf) {
+        int retValue = singleBuf;
+        singleBuf = 0;
+        return retValue;
+    }
+    return getchar();
 }
 void ungetch(int c) {
     if (bufp >= BUFSIZE) {
         printf("ungetch: too many characters\n");
     } else {
-        buf[bufp++] = c;
+        singleBuf = c;
     }
 }
 
