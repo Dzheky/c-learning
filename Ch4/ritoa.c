bfe40a1ac3029ad91ae2e7e552accf9da156d11a
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-05-08 14:37:34 +0100

    4.12

---
+++ Ch4/ritoa.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define MAX_ARR 1000
+
+void ritoa(char [], int);
+
+int main() {
+    char str[MAX_ARR];
+    ritoa(str, 1034);
+    printf("number in string: %s\n", str);
+    return 0;
+}
+
+void ritoa(char s[], int d) {
+    static int i = 0;
+    static int x = 0;
+    i++;
+    if (d != 0) {
+        ritoa(s, d / 10);
+        s[x++] = d % 10 + '0';
+    } else {
+        s[x] = '\0';
+    }
+}
