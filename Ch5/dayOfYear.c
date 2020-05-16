89c6786d72435fcfa1966484fcba6a8e688ad18d
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2020-05-16 10:09:47 +0100

    5.9

--- Ch5/dayOfYear.c
+++ Ch5/dayOfYear.c
@@ -3,7 +3,7 @@
 int day_of_year(unsigned int y, unsigned int m, unsigned int d);
 void month_day(unsigned int year, unsigned int dayofyear, unsigned int * m, unsigned int * d);
 
-static char daytab[2][13] = {
+static int daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
@@ -12,8 +12,8 @@ int main() {
     unsigned int m;
     unsigned int d;
 
-    printf("Day of year: %d\n", day_of_year(2020, 5, 32));
-    month_day(2020, 400, &m, &d);
+    printf("Day of year: %d\n", day_of_year(2020, 5, 16));
+    month_day(2020, 137, &m, &d);
     printf("Day: %d, %d\n", m, d);
 
     return 0;
@@ -21,13 +21,15 @@ int main() {
 
 int day_of_year(unsigned int y, unsigned int m, unsigned int d) {
     int i, leap;
+    int * p;
     if (m > 12 || d > 31) {
         printf("ERROR: The date is wrong\n");
         return -1;
     }
     leap = y % 4 == 0 && (y % 100 != 0 || y % 400 == 0);
+    p = &daytab[leap][1];
     for (i = 1; i < m; i++) {
-        d += daytab[leap][i];
+        d += *p++;
     }
 
     return d;
@@ -35,6 +37,7 @@ int day_of_year(unsigned int y, unsigned int m, unsigned int d) {
 
 void month_day(unsigned int y, unsigned int dayofyear, unsigned int * m, unsigned int * d) {
     int i, leap;
+    int *p;
 
     if (dayofyear > 365) {
         printf("ERROR: there is no such day\n");
@@ -44,8 +47,9 @@ void month_day(unsigned int y, unsigned int dayofyear, unsigned int * m, unsigne
     }
 
     leap = y % 4 == 0 && (y % 100 != 0 || y % 400 == 0);
+    p = &daytab[leap][1];
     for (i = 1; dayofyear > daytab[leap][i]; i++) {
-        dayofyear -= daytab[leap][i];
+        dayofyear -= *p++;
     }
 
     *m = i;
